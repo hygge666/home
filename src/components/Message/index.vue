dd6d03a23bbc5dd3a9ca45bb278ad7d44fc755ff
hygee <127015741+hygge666@users.noreply.github.com>
2023-05-04 23:50:23 +0800

    Update index.vue

--- src/components/Message/index.vue
+++ src/components/Message/index.vue
@@ -3,7 +3,7 @@
   <div class="message">
     <!-- Logo -->
     <div class="logo">
-      <img class="logo-img" src="https://img01.anzhiy.cn/useruploads/0/2023/04/29/644c636230a21.jpg" alt="logo" />
+      <img class="logo-img" src="https://img01.anzhiy.cn/useruploads/156/2023/05/04/64537b1db9a27.jpg" alt="logo" />
       <div class="name text-hidden">
         <span class="bg">{{ siteUrl[0] }}</span>
         <span class="sm">.{{ siteUrl[1] }}</span>
